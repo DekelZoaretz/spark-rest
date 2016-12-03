4e5f13bf9ac5e038542683f8da1496f5cfead5b7
Dekel Zoaretz <dekelzoaretz@gmail.com>
2016-12-03 20:50:45 +0200

    initial commit

---
+++ src/main/java/com/dekelzoaretz/courses/Main.java
@@ -0,0 +1,14 @@
+package com.dekelzoaretz.courses;
+
+import static spark.Spark.get;
+
+/**
+ * Created by dekel on 02/12/2016.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+//        get("/hello", (req, res) -> "Hello World");
+    }
+
+}
